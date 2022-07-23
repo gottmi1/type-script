146531a517bae85eabe769015db464d8b573e10f
gottmi1 <91591041+gottmi1@users.noreply.github.com>
2022-07-24 02:04:47 +0900

    class

--- class/example.js
+++ class/example.js
@@ -1,45 +1,92 @@
 "use strict";
-// class Person {
-//   name;
-//   // 먼저 프로퍼티를 만든다
-//   constructor(name: String) {
-//     this.name = name;
-//   }
-//   // 그 후 constructor로 앞서 만든 prop의 타입을 정해주고 this.prop = prop식으로 변경해줌.
-// }
-// 가장 간단한 형태의 class
-// class Person {
-//   public name: string;
-//   public age: number;
-//   // constructor에는 async를 붙일 수 없음.
-//   constructor(name: string, age?: number) { // private를 설정하면 외부에서 접근할 수 없기 때문에 new를 호출할 수가 없다.
-//     // 옵셔널 타입을 설정하려면, undefined인 상태에도 대비해야 한다.
-//     if (age === undefined) {
-//       this.age = 20
-//     } else {
-//       this.age = age;
-//     }
-//     this.name = name;
-//   }
-// }
-class Person {
-    constructor(_name, _age) {
-        this._name = _name;
-        this._age = _age;
+function Signature() {
+    class Students {
+        constructor() {
+            this.jinwon = "male"; // 꼭 포함해야 할 값일 때는 이렇게 해줄 수 있다.
+        }
     }
-    // prviate항목은 출력은 가능하지만 .으로 접근할 수 없음
-    get name() {
-        //
-        // console.log('getter 실행')
-        return this._name + "kim";
+    const Aclass = new Students();
+    Aclass.mark = "male";
+    Aclass.kim = "male";
+    console.log(Aclass);
+    const Bclass = new Students();
+    Bclass.chloe = "female";
+    Bclass.anna = "female";
+    console.log(Bclass);
+}
+function Static() {
+    class Person {
+        hello() {
+            console.log('hello', Person.CITY);
+        }
+        change() {
+            Person.CITY = 'Seoul';
+        }
     }
-    set name(name) {
-        console.log('setter 실행');
+    Person.CITY = 'Deajeon';
+    const p1 = new Person();
+    const p2 = new Person();
+    p1.hello();
+    p2.hello();
+    p1.change();
+    p2.hello();
+}
+function Singletons() {
+    class ClassName {
+        constructor() { }
+        static getInstance() {
+            // 없으면 만들어서 리턴.
+            if (this.instance === null) {
+                ClassName.instance = new ClassName();
+                // 프라이빗이라 클래스 내에선 new를 사용할 수 있음.
+            }
+            // ClassName으로부터 만든 객체가 있다면 그걸 리턴 === 처음 만들어진 순간부터는 리턴받은 것이 모두 같은 객체라는 뜻
+            return ClassName.instance;
+        }
+    }
+    ClassName.instance = null;
+    // 싱글톤 패턴은 클래스로 오브젝트를 단 하나만 찍어내 사용하는 단일객체 패턴이다.
+    const c1 = ClassName.getInstance();
+    const c2 = ClassName.getInstance();
+    // new로 새로운 객체를 만들면 각각 다른 객체로 판정되기 때문에 싱글톤 패턴에서는 new라는 키워드를 사용하면 안된다.
+    console.log(c1 === c2); //true
+}
+function Inheritance() {
+    class Parent {
+        constructor(_name, _age) {
+            this._name = _name;
+            this._age = _age;
+        }
+        print() {
+            console.log(`이름 ${this._name}, 나이 ${this._age}`);
+        }
+        printName() {
+            console.log(this._name, this._age);
+        }
+    }
+    // const p1 = new Parent("jinwon", 27);
+    // p1.print();
+    class Child extends Parent {
+        constructor(age) {
+            super("Jinwon Jr", age);
+            // 상속받은 클래스의 컨스트럭터에서 부모 클래스의 메서드를 사용하고 싶다면 먼저 super부터 실행해줘야 한다. 왜냐하면 Parent의 this._name이나 _age는 아직 할당이 되지 않았기 때문.
+            this.printName();
+        }
+    }
+    const c1 = new Child(22);
+    c1.print();
+}
+class AbstractPerson {
+    constructor() {
+        this._name = "jinwon";
+        // 메서드를 만들었는데 기능이 없음(완전하지 않음)
+    }
+}
+class Person extends AbstractPerson {
+    setName(name) {
         this._name = name;
     }
 }
-const p1 = new Person("jinwon", 27);
-console.log(p1);
-console.log(p1.name);
-p1.name = "won kim";
-console.log(p1);
+const p = new Person();
+p.setName("jinwon");
+console.log(p);
--- class/example.ts
+++ class/example.ts
@@ -1,52 +1,116 @@
-// class Person {
-//   name;
-//   // 먼저 프로퍼티를 만든다
-//   constructor(name: String) {
-//     this.name = name;
-//   }
-//   // 그 후 constructor로 앞서 만든 prop의 타입을 정해주고 this.prop = prop식으로 변경해줌.
-// }
-// 가장 간단한 형태의 class
-
-// class Person {
-//   public name: string;
-//   public age: number;
-
-//   // constructor에는 async를 붙일 수 없음.
-//   constructor(name: string, age?: number) { // private를 설정하면 외부에서 접근할 수 없기 때문에 new를 호출할 수가 없다.
-//     // 옵셔널 타입을 설정하려면, undefined인 상태에도 대비해야 한다.
-//     if (age === undefined) {
-//       this.age = 20
-//     } else {
-//       this.age = age;
-//     }
-//     this.name = name;
-//   }
-// }
-
-class Person {
-  public readonly name: string = "jinwon";
-  // 퍼블릭이라 밖에서 접근 가능하지만 readonly라서 변경할 수 없게 만든다
-  private readonly country: string;
-  // 프라이빗이라 안에서는 바꿀 수 있어야 하는 상황에서도 readonly로 인해 변경할 수 없다.
-
-  constructor(private _name: string, private _age: number) {
-    this.country = "Korea";
-    // 위에서 선언만 해 놓은 상태이기 때문에 할당만 가능하다.
+function Signature() {
+  class Students {
+    // mark: string = 'male';
+    [index: string]: 'male' | 'female'; // string을 사용해도 되지만 이렇게 하는 게 더 정확함. 
+    jinwon: 'male' = "male"; // 꼭 포함해야 할 값일 때는 이렇게 해줄 수 있다.
   }
-  // prviate항목은 출력은 가능하지만 .으로 접근할 수 없음
-  // get name() {
-  //   return this._name + " kim";
-  // }
-  // set name(name: string) {
-  //   this._name = name;
-  // }
-  // 얘를 실행할 땐 함수를 실행() 시키지 않고 그냥 불러온다.
+
+  const Aclass = new Students();
+  Aclass.mark = "male";
+  Aclass.kim = "male"
+
+  console.log(Aclass);
+
+  const Bclass = new Students();
+  Bclass.chloe = "female";
+  Bclass.anna = "female"
+
+  console.log(Bclass);
 }
 
-const p1 = new Person("jinwon", 27);
+function Static() {
+  class Person {
+    private static CITY = 'Deajeon';
 
-console.log(p1)
-console.log(p1.name);
-// p1.name = "won kim";
-console.log(p1);
+    public hello() {
+      console.log('hello', Person.CITY)
+    }
+    public change() {
+      Person.CITY = 'Seoul'
+    }
+  }
+
+  const p1 = new Person();
+
+  const p2 = new Person();
+
+  p1.hello();
+  p2.hello();
+
+  p1.change()
+  p2.hello();
+}
+
+function Singletons() {
+  class ClassName {
+    private static instance: ClassName | null = null;
+    public static getInstance() {
+      // 없으면 만들어서 리턴.
+      if (this.instance === null) {
+        ClassName.instance = new ClassName();
+        // 프라이빗이라 클래스 내에선 new를 사용할 수 있음.
+      }
+      // ClassName으로부터 만든 객체가 있다면 그걸 리턴 === 처음 만들어진 순간부터는 리턴받은 것이 모두 같은 객체라는 뜻
+      return ClassName.instance;
+    }
+    private constructor() { }
+    // 컨스트럭터에 프라이빗을 달면 밖에서 new로 생성할 수가 없어진다
+  }
+  // 싱글톤 패턴은 클래스로 오브젝트를 단 하나만 찍어내 사용하는 단일객체 패턴이다.
+
+  const c1 = ClassName.getInstance()
+
+  const c2 = ClassName.getInstance()
+  // new로 새로운 객체를 만들면 각각 다른 객체로 판정되기 때문에 싱글톤 패턴에서는 new라는 키워드를 사용하면 안된다.
+
+  console.log(c1 === c2); //true
+}
+
+
+function Inheritance() {
+  class Parent {
+    constructor(protected _name: string, private _age: number) {
+
+    }
+    public print(): void {
+      console.log(`이름 ${this._name}, 나이 ${this._age}`);
+    }
+    protected printName() {
+      console.log(this._name, this._age);
+    }
+  }
+
+  // const p1 = new Parent("jinwon", 27);
+  // p1.print();
+
+  class Child extends Parent {
+    constructor(age: number) {
+      super("Jinwon Jr", age);
+      // 상속받은 클래스의 컨스트럭터에서 부모 클래스의 메서드를 사용하고 싶다면 먼저 super부터 실행해줘야 한다. 왜냐하면 Parent의 this._name이나 _age는 아직 할당이 되지 않았기 때문.
+      this.printName();
+    }
+  }
+
+  const c1 = new Child(22)
+  c1.print();
+}
+
+function Abstract() {
+  abstract class AbstractPerson {
+    protected _name: string = "jinwon";
+
+    // abstract를 사용할 땐 클래스에도 abstract클래스 임을 명시해야 한다.
+    abstract setName(name: string): void;
+    // 메서드를 만들었는데 기능이 없음(완전하지 않음)
+  }
+
+  class Person extends AbstractPerson {
+    setName(name: string): void {
+      this._name = name;
+    }
+  }
+
+  const p = new Person();
+  p.setName("jinwon")
+  console.log(p);
+}
